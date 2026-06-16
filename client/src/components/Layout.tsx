c78d34d4c2867406407a7cb25b946b7edbdc25cb
ashutosh7761 <goluashu3@gmail.com>
2026-06-16 22:42:04 +0530

    add side-bar on dashboard section

---
+++ client/src/components/Layout.tsx
@@ -0,0 +1,13 @@
+// import { Sidebar } from "lucide-react"
+import Sidebar from "./Sidebar"
+
+
+const Layout = () => {
+  return (
+    <div>
+        <Sidebar />
+    </div>
+  )
+}
+
+export default Layout
